111e4c67f9938ac6a469c715e8a88dfd18591b0a
The Hammer of God <2475253520@qq.com>
2025-10-21 17:51:20 +0800

    Create lesson-1-homework.go

---
+++ lesson-1-homework.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello林语杰")
+	fmt.Println("hello" + "林语杰")
+	fmt.Print("hello")
+	fmt.Print("林语杰\n")
+	name := "林语杰"
+	fmt.Printf("Hello, %s!\n", name)
+
+}
