b0067c2828ba2acb4be7feb1ed7eed2e6612aa9c
The Hammer of God <2475253520@qq.com>
2025-10-21 18:23:18 +0800

    Update lesson-1-homework.go
    10.21

--- lesson-1-homework.go
+++ lesson-1-homework.go
@@ -10,4 +10,16 @@ func main() {
 	name := "林语杰"
 	fmt.Printf("Hello, %s!\n", name)
 
+	const pi = 3.14
+	var area float64
+	var r float64 = 5
+	area = pi * r * r
+	fmt.Println("area:", area)
+
+	sum := 0
+	for i := 0; i <= 1000; i++ {
+		sum += i
+		fmt.Println(sum)
+	}
+
 }
