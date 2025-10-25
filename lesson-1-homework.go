7c279170e933b7cafc45b3f2c941c39235302ed5
The Hammer of God <2475253520@qq.com>
2025-10-25 21:45:16 +0800

    Update lesson-1-homework.go
    Final completion

--- lesson-1-homework.go
+++ lesson-1-homework.go
@@ -9,7 +9,13 @@ func fact(n int) int {
 	}
 	return p
 }
+func average(sum int, count int) float64 {
+	var p float64
+	p = float64(sum) / float64(count)
+	return p
+}
 
+// -----------------------------------------------------------
 func main() {
 	fmt.Println("Hello林语杰")
 	fmt.Println("hello" + "林语杰")
@@ -18,11 +24,15 @@ func main() {
 	name := "林语杰"
 	fmt.Printf("Hello, %s!\n", name)
 
+	//-----------------------------------------------------
+
 	const pi = 3.14
 	var area float64
 	var r float64 = 5
 	area = pi * r * r
 	fmt.Println("area:", area)
+
+	//-----------------------------------------------------
 
 	sum := 0
 	for i := 0; i <= 1000; i++ {
@@ -30,9 +40,29 @@ func main() {
 		fmt.Println(sum)
 	}
 
+	//----------------------------------------------------
+
 	var n int
 	fmt.Println("Input:")
 	fmt.Scan(&n)
 	fmt.Println("n! = ", fact(n))
+
+	//----------------------------------------------------
+
+	fmt.Println("输入整数，输入0停止：")
+	var x int = 1
+	var total, count int
+	for x != 0 {
+		fmt.Scan(&x)
+		total = total + x
+		count++
+	}
+	fmt.Println("total:", total, "count:", count)
+	var aver float64 = average(total, count)
+	if aver >= 60 {
+		fmt.Printf("平均成绩为%.2f , 合格！", aver)
+	} else {
+		fmt.Printf("平均成绩为%.2f , 不合格!", aver)
+	}
 
 }
